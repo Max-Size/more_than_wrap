5a59991f26f7afa9a84881b5c1421f421e150d98
Maxim Seleznev <maximuhkass14@gmail.com>
2025-07-01 16:05:45 +0300

    added example of usage

--- lib/more_than_wrap.dart
+++ lib/more_than_wrap.dart
@@ -1,8 +1,1 @@
-import 'dart:math';
-
-import 'package:flutter/material.dart';
-import 'package:flutter/rendering.dart';
-
-part 'implementation/render_widget.dart';
-part 'implementation/render_box.dart';
-part 'implementation/widget.dart';
+export 'src/widget.dart';
